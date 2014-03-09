8eb7f66a21a3916a480bf4529564b77fbb151d3d
liyicky <liyicky@gmail.com>
2014-03-09 13:01:33 -0400

    Clumps form css

--- app/models/clump.rb
+++ app/models/clump.rb
@@ -1,4 +1,4 @@
 class Clump < ActiveRecord::Base
-  validates :url, :shit, presence: true
-  validates :url, :format => URI::regexp(%w(http https))
+  validates :url, presence: true
+#  validates :url, :format => URI::regexp(%w(http https))
 end
